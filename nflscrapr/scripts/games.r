d7ca6995d228a402ba59d6c7f9ec91650e1a69dd
shind-ub <scott.hind@unbounce.com>
2019-11-04 19:31:48 -0800

    Write to temporary folder

--- nflscrapr/scripts/games.r
+++ nflscrapr/scripts/games.r
@@ -5,13 +5,14 @@ suppressMessages(library("optparse"))
 main <- function() {
     # Argument parsing
     option_list <- list(
-        make_option(c("-y", "--year"), type="character", default=2019, 
-                help="season year", metavar="character"),
-        make_option(c("-w", "--week"), type="character", default=NULL, 
-                help="week of games", metavar="character"),
         make_option(c("-t", "--type"), type="character", default="reg", 
-                help="week of games", metavar="character")
-    ); 
+            help="week of games", metavar="character"),
+        make_option(c("-y", "--year"), type="character", default=2019, 
+            help="season year", metavar="character"),
+        make_option(c("-w", "--week"), type="character", default=NULL, 
+            help="week of games", metavar="character")
+    ) 
+
     opt_parser <- OptionParser(option_list=option_list);
     args <- parse_args(opt_parser);
 
@@ -19,30 +20,20 @@ main <- function() {
         stop("year argument not supplied.")
     }
 
-    # if (is.null(args$week)) {
-    #     stop("week argument not supplied.")
-    # }
+    if (is.null(args$type)) {
+        stop("game type argument not supplied.")
+    }
 
     year <- args$year
-    #week <- args$week
+    week <- args$week
     game_type <- args$type
 
-    # if (typeof(week) != "integer") {
-    #     week <- as.integer(week)
-    # }
-
     # Get games for week
-    games <- scrape_game_ids(year)
-    csv_name <- "/app/data/games.csv"
+    games <- scrape_game_ids(year, type=game_type)
+    csv_name <- "/app/data/games_dump.csv"
 
-    # if (file.exists(csv_name)) {
-    #     append_mode <- TRUE
-    # }
-    # else {
-    #     append_mode <- FALSE
-    # }
-
-    write.table(games, sep=",", file=csv_name, col.names=!file.exists(csv_name), row.names=FALSE, append=file.exists(csv_name))
+    #write.table(games, sep=",", file=csv_name, col.names=!file.exists(csv_name), row.names=FALSE, append=file.exists(csv_name))
+    write.table(games, sep=",", file=csv_name, col.names=TRUE, row.names=FALSE, append=FALSE)
 
 }
 
