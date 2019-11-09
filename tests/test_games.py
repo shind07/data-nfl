863f8b0263779dab2e93a60f49f12eebd9747adb
shind07 <shind@sfu.ca>
2019-11-09 10:55:09 -0800

    Update test coverage

--- tests/test_games.py
+++ tests/test_games.py
@@ -5,7 +5,7 @@ from unittest.mock import MagicMock, patch
 
 import pandas as pd
 
-from jobs import games
+from jobs import games, config
 
 logger = logging.getLogger('test_logger')
 logger.setLevel(logging.DEBUG)
@@ -23,25 +23,27 @@ class TestGames(unittest.TestCase):
     
     def test_data_integrity_check(self):
         """
-        3 conditions must be met:
+        4 conditions must be met:
         - there must not be any gaps in the seasons
         - seasons must have all 3 season types (except most recent season)
         - seasons must have semantic ordering of season types (pre -> reg -> post)
+        - data must have weeks 1-17 if type is reg
         """
         with self.assertRaises(ValueError) as cm:
-                games.data_integrity_check("not a dataframe")
+            games.data_integrity_check("not a dataframe")
         logger.debug(cm.exception)
 
         good_dfs = [
             self.test_df[(self.test_df['season'] == 2011) & (self.test_df['type'] != 'post')],
-            self.test_df[(self.test_df['season'] == 2011) & (self.test_df['type'] != 'post') & (self.test_df['type'] != 'reg')]
+            self.test_df[(self.test_df['season'] == 2011) & (self.test_df['type'] == 'pre')]
         ]
 
         for good_df in good_dfs:
             exception_raised = False
             try:
                 games.data_integrity_check(good_df)
-            except:
+            except Exception as e:
+                logging.debug(e)
                 exception_raised = True
             self.assertFalse(exception_raised)
 
@@ -50,7 +52,8 @@ class TestGames(unittest.TestCase):
             self.test_df[(self.test_df['season'] == 2011) & (self.test_df['type'] != 'reg')],
             self.test_df[(self.test_df['season'] != 2011) | (self.test_df['type'].isin(['reg', 'post']))],
             self.test_df[(self.test_df['season'] == 2011) & (~self.test_df['type'].isin(['reg', 'pre']))],
-            self.test_df[self.test_df['season'] != 2012]
+            self.test_df[self.test_df['season'] != 2012],
+            self.test_df[(self.test_df['season'] == 2011) & (self.test_df['type'].isin(['reg', 'pre'])) & (self.test_df['week'] != 2)]
         ]
 
         for bad_df in bad_dfs:
@@ -81,17 +84,25 @@ class TestGames(unittest.TestCase):
     def test_get_next_season_and_type(self):
         """Test that function can order season types semantically"""
         results = []
-        for season_type in games.SEASON_TYPES:
+        for season_type in config.SEASON_TYPES:
             results.append(games.get_next_season_and_type(2011, season_type))
         
         self.assertEqual(results[0], (2011, 'reg'))
         self.assertEqual(results[1], (2011, 'post'))
         self.assertEqual(results[2], (2012, 'pre'))
 
-        grid = games.get_next_season_and_type(games.CURRENT_SEASON, 'post')
-        self.assertEqual(len(grid), 0)
+        next_season_and_type = games.get_next_season_and_type(config.CURRENT_SEASON, 'post')
+        self.assertIsNone(next_season_and_type)
 
-        
+        next_season_and_type = games.get_next_season_and_type(config.START_SEASON, 'pre', order='prev')
+        self.assertIsNone(next_season_and_type)
+
+        next_season_and_type = games.get_next_season_and_type(config.CURRENT_SEASON, 'post', order='prev')
+        self.assertEqual(next_season_and_type, (config.CURRENT_SEASON, 'reg'))
+
+        next_season_and_type = games.get_next_season_and_type(config.CURRENT_SEASON, 'pre', order='prev')
+        self.assertEqual(next_season_and_type, (config.CURRENT_SEASON - 1, 'post'))
+
     def test_get_seasons_grid(self):
         """Test that the grid is properly populated"""
         with self.assertRaises(ValueError) as cm:
@@ -112,7 +123,7 @@ class TestGames(unittest.TestCase):
         self.assertEqual(first_season_type, 'pre')
 
         latest_season, latest_season_type = grid[-1]
-        self.assertEqual(latest_season, games.CURRENT_SEASON)
+        self.assertEqual(latest_season, config.CURRENT_SEASON)
         self.assertEqual(latest_season_type, 'post')
 
         grid = games.get_seasons_grid(2013, 'pre')
@@ -130,13 +141,13 @@ class TestGames(unittest.TestCase):
         self.assertEqual(first_season, 2013)
         self.assertEqual(first_season_type, 'post')
 
-        grid = games.get_seasons_grid(games.START_SEASON, 'pre')
+        grid = games.get_seasons_grid(config.START_SEASON, 'pre')
         seasons = set([season_and_type[0] for season_and_type in grid])
-        expected_number_of_seasons = len(list(range(games.START_SEASON, games.CURRENT_SEASON + 1)))
-        self.assertEqual(min(seasons), games.START_SEASON)
-        self.assertEqual(max(seasons), games.CURRENT_SEASON)
+        expected_number_of_seasons = len(list(range(config.START_SEASON, config.CURRENT_SEASON + 1)))
+        self.assertEqual(min(seasons), config.START_SEASON)
+        self.assertEqual(max(seasons), config.CURRENT_SEASON)
         self.assertEqual(len(seasons), expected_number_of_seasons)
-        self.assertEqual(len(grid), expected_number_of_seasons*len(games.SEASON_TYPES))
+        self.assertEqual(len(grid), expected_number_of_seasons*len(config.SEASON_TYPES))
 
 if __name__ == '__main__':
     unittest.main()
