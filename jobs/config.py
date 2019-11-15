e241501d93705ba5465c797cdf009c610aca0a35
shind07 <shind@sfu.ca>
2019-11-14 21:05:51 -0800

    Add more config variables

--- jobs/config.py
+++ jobs/config.py
@@ -4,26 +4,14 @@ START_SEASON, CURRENT_SEASON = (2011, 2019)
 SEASON_TYPES = ('pre', 'reg', 'post')
 SEASON_TYPES_ORDER = {game_type: order for order, game_type in enumerate(SEASON_TYPES)}
 
-DATA_DIR = os.path.join(
-    os.path.dirname(  # /data-nfl
-        os.path.dirname(  # /jobs
-            os.path.abspath(__file__)
-        )
-    ),
-    "data"
+root_dir = os.path.dirname(  # /data-nfl
+    os.path.dirname(  # /jobs
+        os.path.abspath(__file__)
+    )
 )
-DUMP_CSV_NAME = 'games_dump.csv'
-DUMP_CSV_PATH = os.path.join(DATA_DIR, DUMP_CSV_NAME)
-GAMES_CSV_PATH = os.path.join(DATA_DIR, "games.csv")
 
-DOCKER_CONTAINER = 'nflscrapr'
-DOCKER_CONTAINER_ARGS = {
-    "volumes": {  # equivalent to -v ${PWD}/data:/app/data  $(NFLSCRAPR_APP_NAME)
-        DATA_DIR: {
-            "bind": '/app/data',
-            'mode': 'rw'
-        }
-    },
-    "tty": True,
-    "stdout": True
-}
+DATA_DIR = os.path.join(root_dir, "data")
+GAMES_DUMP_CSV_NAME = 'games_dump.csv'
+GAMES_DUMP_CSV_PATH = os.path.join(DATA_DIR, GAMES_DUMP_CSV_NAME)
+GAMES_CSV_PATH = os.path.join(DATA_DIR, "games.csv")
+NFLSCRAPR_JOBS_PATH = os.path.join(root_dir, "nflscrapr")
