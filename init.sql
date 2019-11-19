92c32045b4a643ae030215c644b07cead06ad3b0
shind07 <shind@sfu.ca>
2019-11-19 05:33:37 -0800

    Remove backticks

--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS games 
   ( 
-     `type`          VARCHAR(16) NOT NULL, 
-     `game_id`       INTEGER PRIMARY KEY, 
-     `home_team`     VARCHAR(16) NOT NULL, 
-     `away_team`     VARCHAR(16) NOT NULL, 
-     `week`          SMALLINT NOT NULL, 
-     `season`        SMALLINT NOT NULL, 
-     `state_of_game` VARCHAR(16) NOT NULL, 
-     `game_url`      VARCHAR(255) NOT NULL, 
-     `home_score`    SMALLINT, 
-     `away_score`    SMALLINT 
+     type          VARCHAR(16) NOT NULL, 
+     game_id       INTEGER PRIMARY KEY, 
+     home_team     VARCHAR(16) NOT NULL, 
+     away_team     VARCHAR(16) NOT NULL, 
+     week          SMALLINT NOT NULL, 
+     season        SMALLINT NOT NULL, 
+     state_of_game VARCHAR(16) NOT NULL, 
+     game_url      VARCHAR(255) NOT NULL, 
+     home_score    SMALLINT, 
+     away_score    SMALLINT 
   );
 
 
