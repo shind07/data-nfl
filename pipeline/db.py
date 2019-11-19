612b000eebffb66ef6f48f7c72a7a038bff284bc
shind07 <shind@sfu.ca>
2019-11-19 05:34:06 -0800

    Helper functions for sqlalchemy

---
+++ pipeline/db.py
@@ -0,0 +1,26 @@
+import sqlalchemy as db
+
+PG_USERNAME = 'postgres'
+PG_PASSWORD = 'password'
+PG_HOST = 'postgres'
+DB_NAME = 'nfl'
+
+
+def get_connection_string():
+    return f"postgresql://{PG_USERNAME}:{PG_PASSWORD}@{PG_HOST}/{DB_NAME}"
+
+
+def connect_to_db(connection_string):
+    """Connect to a db with the given connection string
+
+    Example usage:
+        conn_str = db.get_connection_string()
+        conn = db.connect_to_db(conn_str)
+        results = conn.execute('select * from database')
+    
+    :param connection_string: [description]
+    :type connection_string: [type]
+    :return: [description]
+    :rtype: [type]
+    """
+    return db.create_engine(connection_string).connect()
