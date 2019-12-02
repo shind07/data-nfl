7b3fea8057f716a93d939760a3deced40452727a
shind07 <shind@sfu.ca>
2019-12-01 22:32:38 -0800

    allow for imports from parent dir

---
+++ app/pipeline/__init__.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# allow the pipeline to import modules rom parent dir (specifically db.py)
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
