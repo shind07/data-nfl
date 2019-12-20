c58e49d04575a8117f55da2fc4de7e89717013e8
shind07 <shind@sfu.ca>
2019-12-19 17:14:00 -0800

    add play by play config

--- app/app/pipeline/config.py
+++ app/app/pipeline/config.py
@@ -10,7 +10,12 @@ START_SEASON, CURRENT_SEASON = (2011, 2019)
 SEASON_TYPES = ('pre', 'reg', 'post')
 SEASON_TYPES_ORDER = {game_type: order for order, game_type in enumerate(SEASON_TYPES)}
 DATA_DIR = os.path.join(root_dir, "data")
+NFLSCRAPR_JOBS_PATH = os.path.join(root_dir, "nflscrapr")
+
 GAMES_DUMP_CSV_NAME = 'games_dump.csv'
 GAMES_DUMP_CSV_PATH = os.path.join(DATA_DIR, GAMES_DUMP_CSV_NAME)
 GAMES_CSV_PATH = os.path.join(DATA_DIR, "games.csv")
-NFLSCRAPR_JOBS_PATH = os.path.join(root_dir, "nflscrapr")
+
+PLAY_BY_PLAY_DUMP_CSV_NAME = 'play_by_play_dump.csv'
+PLAY_BY_PLAY_DUMP_CSV_PATH = os.path.join(DATA_DIR, PLAY_BY_PLAY_DUMP_CSV_NAME)
+PLAY_BY_PLAY_CSV_PATH = os.path.join(DATA_DIR, "play_by_play.csv")
